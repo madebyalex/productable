7756126c1a9ee45a87c1273f5cba69ac04107562
Alexander <hello@alexander.works>
2020-07-31 20:46:15 +0300

    Initial Commit

---
+++ Productable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Productable
+//
+//  Created by Alexander on 31.07.2020.
+//  Copyright © 2020 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
