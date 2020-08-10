b6cc6a9708e865c06802eb887de04707d7a801b1
Alexander <hello@alexander.works>
2020-08-10 17:31:23 +0300

    Fixed the issue with viewing the app in popup

---
+++ Productable/Controller/RegisterVC.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterVC.swift
+//  Productable
+//
+//  Created by Alexander on 02.08.2020.
+//  Copyright © 2020 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+    
+    // Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- Productable/Controller/ViewController.swift
+++ Productable/Controller/ViewController.swift
@@ -12,12 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         let storyboard = UIStoryboard(name: "LoginStoryboard", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "loginVC")
+        controller.modalPresentationStyle = .fullScreen
         present(controller, animated: true, completion: nil)
     }
 
